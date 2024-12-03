27a479a8a225050210621ea4b07fe8d3b3eeaf1d
sapperEduardo <166638298+sapperEduardo@users.noreply.github.com>
2024-12-03 20:25:41 -0300

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 
-DB="gimnasio"
+DB="gimnasio_7zpe"
 USER="seba_render"
-PASSWORD="admin"
-HOST="localhost"
+PASSWORD="mVmL71r995jKRS2SKvN6t92BSRLSob4V"
+HOST="dpg-ct7oi7dds78s73985ia0-a"
 PORT="5432"
