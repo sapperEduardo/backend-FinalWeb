e7b05fe899980fec0e209c8f8b1669b0e07680e6
sapperEduardo <166638298+sapperEduardo@users.noreply.github.com>
2024-12-05 02:21:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,29 +7,28 @@ from routers.reserva_rutes import reserva_router
 
 app = FastAPI()
 
-
+# Lista de orígenes permitidos. Añade más dominios si es necesario.
 origins = [
-    "http://127.0.0.1:5500",  # Permite tu origen de desarrollo
-    "http://localhost:5500",  # Alternativa por si usas localhost
-    # Añade otros orígenes si los necesitas
+    "http://127.0.0.1:5500",   # Desarrollo local
+    "http://localhost:5500",   # Alternativa localhost
+    "https://gymfitgym.netlify.app",  # Dominio de tu aplicación frontend en producción
+    "https://backend-finalweb.onrender.com"  # Dominio del backend en producción
 ]
 
-# Configura el middleware CORS
+# Configuración del middleware CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],# Permitir los orígenes especificados
-    allow_credentials=True,            # Permitir el uso de cookies
-    allow_methods=["*"],               # Permitir todos los métodos (GET, POST, etc.)
-    allow_headers=["*"],               # Permitir todos los encabezados
+    allow_origins=origins,  # Permitir los orígenes especificados
+    allow_credentials=True,  # Permitir el uso de cookies
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Métodos específicos
+    allow_headers=["Content-Type", "Authorization"],  # Encabezados específicos
 )
-
 
 @app.get("/")
 def get():
-    return {"Estado":"Correcto!"}
+    return {"Estado": "Correcto!"}
 
-
+# Registrar los routers
 app.include_router(user_router)
 app.include_router(horario_router)
 app.include_router(reserva_router)
-
